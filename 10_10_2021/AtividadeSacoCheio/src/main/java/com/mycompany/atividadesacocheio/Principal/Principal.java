e5ff9888dba7ed9027c26590bf771e9f8e02dac8
Gabriel Evangelista Pinheiro <89315231+Gabaraael@users.noreply.github.com>
2021-10-12 17:06:55 -0300

    Atividade DAO, DOT, MENU em Java.

--- 10_10_2021/AtividadeSacoCheio/src/main/java/com/mycompany/atividadesacocheio/Principal/Principal.java
+++ 10_10_2021/AtividadeSacoCheio/src/main/java/com/mycompany/atividadesacocheio/Principal/Principal.java
@@ -3,7 +3,7 @@ package com.mycompany.atividadesacocheio.Principal;
 import com.mycompany.atividadesacocheio.Service.FuncionarioService;
 
 public class Principal {
-
+    
     public static void main(String Args[]) {
         FuncionarioService fun = new FuncionarioService();
             fun.exibirMenu();
