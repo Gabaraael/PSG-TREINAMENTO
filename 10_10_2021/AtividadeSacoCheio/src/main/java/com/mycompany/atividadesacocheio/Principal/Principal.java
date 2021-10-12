40d468c3663ebb51ace28269a18c41aff8cd817f
Gabriel Evangelista Pinheiro <89315231+Gabaraael@users.noreply.github.com>
2021-10-12 17:05:36 -0300

    Update  Exception

--- 10_10_2021/AtividadeSacoCheio/src/main/java/com/mycompany/atividadesacocheio/Principal/Principal.java
+++ 10_10_2021/AtividadeSacoCheio/src/main/java/com/mycompany/atividadesacocheio/Principal/Principal.java
@@ -1,13 +1,10 @@
 package com.mycompany.atividadesacocheio.Principal;
 
-
 import com.mycompany.atividadesacocheio.Service.FuncionarioService;
-import java.text.ParseException;
-
 
 public class Principal {
 
-    public static void main(String Args[]) throws ParseException {
+    public static void main(String Args[]) {
         FuncionarioService fun = new FuncionarioService();
             fun.exibirMenu();
 
